6f9dd897a0fc34b4c005651056ebfabcc68259ab
Ivan Lara <uki@live.co.uk>
2022-06-04 17:47:13 -0600

    app background fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Card } from "./components/card/Card";
 
 function App() {
     return (
-        <div className="flex items-center justify-center h-screen w-full bg-waves bg-no-repeat bg-light-purple">
+        <div className="flex items-center justify-center h-screen w-full bg-waves bg-contain bg-no-repeat bg-light-purple">
             <Card />
         </div>
     );
